1ec45226887575b3bb1a047d48e73b58e0cc3d62
d_liyanage <admliyanage@gmail.com>
2021-01-12 22:21:11 +0530

    backend finished

--- mysql.py
+++ mysql.py
@@ -19,5 +19,37 @@ def get_all_tasks():
     rv = cur.fetchall()
     return jsonify(rv)
 
+@app.route('/api/task', methods=['POST'])
+def add_task():
+    cur = mysql.connection.cursor();
+    task = request.get_json()['task']
+    cur.execute("INSERT INTO vue_flask_todo.tasks (task) VALUES ('"+ str(task) +"') ")
+    mysql.connection.commit()
+    result = {'task' : task}
+    return jsonify({'result': result})
+    
+@app.route('/api/task/<id>', methods=['PUT'])
+def update_task(id):
+    cur = mysql.connection.cursor()
+    task = request.get_json()['task']
+    cur.execute("UPDATE vue_flask_todo.tasks SET task = '"+ str(task)  +"' WHERE id = " + id)
+    mysql.connection.commit()
+    result = {'task':task}
+    return jsonify({'result': result})
+
+@app.route('/api/task/<id>',methods=['DELETE'])
+def delete_task(id):
+    cur = mysql.connection.cursor()
+    response = cur.execute("DELETE FROM vue_flask_todo.tasks WHERE id ="+id)
+    mysql.connection.commit()
+    
+    if response > 0 :
+        result = { 'message': 'record deleted'}
+    else:
+        result = {'message': 'no record found'}
+    
+    return jsonify({'result': result})
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
